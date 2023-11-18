bc2c7d9d10b587a46b99c74ac413c93b9a7b8cb8
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-11-18 18:39:23 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:prj_routing/home_screen.dart';
+import 'package:prj_routing/second_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeScreen(),
+      '/second' :(context) => const SecondScreen(),
+
+    },
+
+  )
+  );
+}
+
